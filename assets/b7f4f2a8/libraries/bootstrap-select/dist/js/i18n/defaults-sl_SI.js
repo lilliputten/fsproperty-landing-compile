304ba347a307fd06c3c6938d47629593293af262
lilliputten <lilliputten@gmail.com>
2024-10-14 19:44:22 +0300

    Build publish.0.0.7, 2024.10.14 19:42 +0300 (241014-1942)

---
+++ assets/b7f4f2a8/libraries/bootstrap-select/dist/js/i18n/defaults-sl_SI.js
@@ -0,0 +1,7 @@
+/*!
+ * Bootstrap-select v1.6.3 (http://silviomoreto.github.io/bootstrap-select)
+ *
+ * Copyright 2013-2015 bootstrap-select
+ * Licensed under MIT (https://github.com/silviomoreto/bootstrap-select/blob/master/LICENSE)
+ */
+jQuery.fn.selectpicker.defaults={noneSelectedText:"Nič izbranega",noneResultsText:"Ni zadetkov za {0}",countSelectedText:function(e,t){},maxOptionsText:function(e,t){return["Omejitev dosežena (max. izbranih: {n})","Omejitev skupine dosežena (max. izbranih: {n})"]},selectAllText:"Izberi vse",deselectAllText:"Počisti izbor",multipleSeparator:", "};
