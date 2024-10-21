29b2b03a0c60379c07337e147ae0b3ac4b48217e
lilliputten <lilliputten@gmail.com>
2024-10-21 19:38:59 +0300

    Build publish.0.0.15, 2024.10.21 19:36 +0300 (241021-1936)

---
+++ uploads/landing-for-owners/mail-config-override.SAMPLE.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @descr Override mail configuration for the specific environment
+ * @changed 2024.10.21, 12:03
+ */
+
+// $gcaptchaServerKey = '...';
+//
+// $toEmailProduction = [
+//   'anybody@mail.com',
+// ];
+//
+// $mailSubject = 'Message subject';
+//
+// // etc (see all the overridable variables in `mail-config.php`)...
--- uploads/landing-for-owners/mail-config.php
+++ uploads/landing-for-owners/mail-config.php
@@ -1,12 +1,20 @@
 <?php
 /**
  * @descr Mail configuration
- * @changed 2024.10.16, 20:21
+ * @changed 2024.10.21, 12:43
  */
 
+/** Google captch secret key */
+$gcaptchaServerKey = '6LdmGmMqAAAAAII1IhWwOlPfFnTNtXPgNBORvpEN'; // DEBUG: By Igor, from wordwizzz, 2024.10.16 (ok works)
+
+/** Return error if sent from another address */
+$checkPageUrl = 'https://fsproperty.ru/';
+
+/** From email name and address */
 $fromName = 'FS-Property';
 $fromEmail = 'fsp@fsproperty.ru';
 
+/* Target address(es) */
 $toEmailDebug = 'dmia@yandex.ru';
 $toEmailProduction = [
   'fsp@fsproperty.ru',
@@ -14,17 +22,30 @@ $toEmailProduction = [
   $toEmailDebug,
 ];
 
+/** Message title */
 $mailSubject = 'Заявка с сайта';
 
+/** Variable names for the representation in a message */
 $formFields = array(
+  // Core variables...
   'name' => 'Имя',
   'email' => 'E-mail',
   'phone' => 'Телефон',
   'comment' => 'Комментарий',
+  'text' => 'Текст',
+  // Service (technical) variables (see `$specialFields` in the main module)...
   '_pageId' => 'Страница',
   '_pageUrl' => 'Адрес страницы на сайте',
   '_ip' => 'IP адрес',
   '_date' => 'Дата',
 );
 
+/** These fields will be checked and script won't process in case of their absence */
+$requiredFields = array(
+  'name',
+  'email',
+  'phone',
+);
+
+/** Fields that require a special processing as multiline */
 $multilineFields = ['comment'];
